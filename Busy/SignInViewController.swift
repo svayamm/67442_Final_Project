a4e933db7acc8fbc9ba4c10d05a405675b19d1f9
Svayam Mishra <svayamm@andrew.cmu.edu>
2016-11-27 19:20:49 -0500

    Svayam: Sign-in phase 2 completed
    Simulator not run - check if ‘framework not found MachO’ error still
    occurring.

--- Busy/SignInViewController.swift
+++ Busy/SignInViewController.swift
@@ -8,12 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SignInViewController: UIViewController, GIDSignInUIDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        GIDSignIn.sharedInstance().uiDelegate = self
+        
+        // Uncommented to automatically (silently) sign in the user.
+        /* Note: When users silently sign in, the Sign-In SDK automatically acquires 
+           access tokens and automatically refreshes them when necessary */
+        GIDSignIn.sharedInstance().signInSilently()
+        
+        // TODO(developer) Configure the sign-in button look/feel
     }
+    
+    /* If you want to customize the button, do the following:
+     In your view controller, declare the sign-in button as a property.
+     @IBOutlet weak var signInButton: GIDSignInButton!
+     Connect the button to the signInButton property you just declared.
+     Customize the button by setting the properties of the GIDSignInButton object. */
+    
+    /* add sign out method to view w/ sign out button
+     @IBAction func didTapSignOut(sender: AnyObject) {
+     GIDSignIn.sharedInstance().signOut()
+     }*/
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
