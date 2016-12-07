5cf589018ef4a8b9fc570ee70c0e9bba1c19477b
j w <jw@js-Mac.local>
2016-12-07 13:52:04 -0500

    Johnny: added some skeleton code for the agenda segmented control and segues, left them commented out since some things it relies on haven't been built out yet

--- Busy/ViewController.swift
+++ Busy/ViewController.swift
@@ -23,7 +23,40 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    // MARK: - Segmented Control
+    
+    //for the segmented control on top
+//    @IBAction func indexChanged(sender:UISegmentedControl)
+//    {
+//        switch segmentedControl.selectedSegmentIndex
+//        {
+//        case 0:
+//            print("Today selected");
+//        case 1:
+//            print("This Week selected");
+//        case 2:
+//            print("All Items selected");
+//        default:
+//            break;
+//        }
+//    }
+    
+    // MARK: - Agenda Page Segues
+    //For the bottom toolbar, commented out since not hooked up to anything yet
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showProjects" {
+            let showProjects:ProjectsViewController = segue.destinationViewController as! ProjectsViewController
+        }
+//        if segue.identifier == "showNew" {
+//            let showNew:NewViewController = segue.destinationViewController as! NewViewController
+//        }
+//        if segue.identifier == "showArchive" {
+//            let showArchive:ArchiveViewController = segue.destinationViewController as! ArchiveViewController
+//        }
+//        if segue.identifier == "showSettings" {
+//            let showSettings:SettingsViewController = segue.destinationViewController as! SettingsViewController
+//        }
+    }
 }
 
