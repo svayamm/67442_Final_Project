a7dffd3649e1ac97fa71a2f266cd6073ecb9a386
j w <jw@js-Mac.local>
2016-12-09 05:08:50 -0500

    Johnny: added segment action

--- Busy/ViewController.swift
+++ Busy/ViewController.swift
@@ -50,8 +50,21 @@ class ViewController: UIViewController {
         })
     }
     
+    @IBAction func indexChanged(sender:UISegmentedControl) {
+        switch timeframeSegment.selectedSegmentIndex {
+            case 0:
+                print("first")
+            case 1:
+                print("second")
+            case 2:
+                print("third")
+            default:
+                break;
+        }
+    }
+    
 //    override func viewDidDisappear(_ animated: Bool) {
-//        <#code#>
+//
 //    }
     
 //    @IBAction func buttonPressed(_ sender: Any) {
