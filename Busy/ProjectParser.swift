67b318dd41b4d1a5b557606bf43d39446d0aebaf
Svayam Mishra <svayamm@andrew.cmu.edu>
2016-12-08 21:42:35 -0500

    Svayam: Started parser code
    Probably will shift this to viewmodels later

---
+++ Busy/ProjectParser.swift
@@ -0,0 +1,28 @@
+//
+//  ProjectJSONParser.swift
+//  Busy
+//
+//  Created by j w on 12/7/16.
+//  Copyright © 2016 Svayam Mishra. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class ProjectParser {
+    
+    class func getData(){
+        
+    }
+    
+//    class func parse(url: String){
+//        let theURL: NSURL = NSURL(string: url)!
+//        
+//        let data = NSData(contentsOf: theURL as URL)!
+//        let swiftyjson = JSON(data: data as Data)
+//        
+//        //        if let owner_login = swiftyjson["items"][0]["owner"]["login"].string {
+//        //            print(owner_login)
+//        //        }
+//    }
+}
---
+++ Busy/TaskParser.swift
@@ -0,0 +1,28 @@
+//
+//  TaskJSONParser.swift
+//  Busy
+//
+//  Created by j w on 12/7/16.
+//  Copyright © 2016 Svayam Mishra. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class TaskParser {
+    
+    class func getData(){
+        
+    }
+    
+//    class func parse(url: String){
+//        let theURL: NSURL = NSURL(string: url)!
+//        
+//        let data = NSData(contentsOf: theURL as URL)!
+//        let swiftyjson = JSON(data: data as Data)
+//        
+//        //        if let owner_login = swiftyjson["items"][0]["owner"]["login"].string {
+//        //            print(owner_login)
+//        //        }
+//    }
+}
