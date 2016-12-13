3dda70f8a2e0cfe2551aeef3dfbe880eae8392d3
j w <jw@js-Mac.local>
2016-12-13 15:59:44 -0500

    Johnny: added more code to projeclistcontroller

--- Busy/ProjectsViewController.swift
+++ Busy/ProjectsViewController.swift
@@ -18,10 +18,43 @@ import Firebase
 class ProjectsViewController: UIViewController {
     let projectsRef = FIRDatabase.database().reference().child("projects").child((FIRAuth.auth()?.currentUser?.uid)!)
     var projectList = [String:AnyObject]()
+    @IBOutlet var tableView: UITableView?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.tableView?.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.projectList.count;
+    }
+    func parseProjectList(){
+        var b = [String:[String:String]]()
+        var c = [String]()
+        for project in projectList{
+            let title = project["title"]
+            b[title] = project.value
+            //b = b.sort()
+            var indexKey = 0
+            for sortedTitle in b {
+                c.append(sortedTitle)
+                indexKey+=1
+            }
+        }
+    }
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        //var cell:UITableViewCell = (self.tableView?.dequeueReusableCell(withIdentifier: "cell"))! as UITableViewCell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath as IndexPath) as! ProjectTableViewCell
+        let proj = projectList[indexPath.row] //as! Project
+        cell.Due = proj.deadline
+        cell.Title = proj.name
+        cell.taskCount = proj.tasks.count
+        //cell.textLabel?.text = self.projectList[indexPath.row]
+        
+        return cell
+    }
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        print("You selected cell #\(indexPath.row)!")
     }
     override func viewDidAppear(_ animated: Bool) {
         projectsRef.observe(FIRDataEventType.value, with: { (snapshot) in
